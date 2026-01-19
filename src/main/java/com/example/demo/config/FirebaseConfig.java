da42e35dc74f5f8f543b02385f2acd1ed8c99285
Miguel Ángel Cámara Casado <miguelangelcamaracasado@gmail.com>
2026-01-19 22:54:47 +0100

    cambio del config de firebase

--- src/main/java/com/example/demo/config/FirebaseConfig.java
+++ src/main/java/com/example/demo/config/FirebaseConfig.java
@@ -5,24 +5,32 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
+import jakarta.annotation.PostConstruct; // Spring Boot 3 usa jakarta en lugar de javax
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
     public void init() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/backend-juegos-firebase-adminsdk-fbsvc-bd45784a28.json");
+        String firebaseJson = System.getenv("FIREBASE_CONFIG");
 
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+        if (firebaseJson != null && !firebaseJson.isEmpty()) {
+            InputStream serviceAccount = new ByteArrayInputStream(firebaseJson.getBytes(StandardCharsets.UTF_8));
 
-        if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } else {
+            // Si no existe (Local), podrías dejar tu código antiguo o un aviso
+            System.out.println("ADVERTENCIA: Variable FIREBASE_CONFIG no encontrada. Ignorando si estás en Local.");
         }
     }
 }
